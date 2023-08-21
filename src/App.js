f362019c8502f502ec6ff86a97e2798a5a57c692
burchakov1998 <124458140+burchakov1998@users.noreply.github.com>
2023-08-21 17:29:28 +0300

    modifed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+const now = new Date(2017, 2, 8);
+
+
+function Calendar (){
+  console.log(now)
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+);
 }
 
 export default App;
